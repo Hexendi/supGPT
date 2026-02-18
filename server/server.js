2e70f1b98db76e68aae1e0f594e647d93b2113e1
Abdellahi N`Diaye <abdellahi.ndiiaye@gmail.com>
2026-02-18 16:56:27 +0000

    bottest

--- server/server.js
+++ server/server.js
@@ -67,11 +67,13 @@ app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get("", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
+
+
 
 
 app.post("/api/ask", async (req, res) => {
