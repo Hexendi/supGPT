5a58ec4aa400721466347b89c02c57fe11ca566c
Abdellahi N`Diaye <abdellahi.ndiiaye@gmail.com>
2026-02-18 17:52:46 +0000

    ai

--- server/server.js
+++ server/server.js
@@ -69,9 +69,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
+
 
 
 
@@ -113,5 +111,7 @@ app.post("/api/ask", async (req, res) => {
     });
   }
 });
-
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 app.listen(5000, () => console.log("Server running on port 5000"));
