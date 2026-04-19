bb74f20be360e4bde1082a10b2227978c84e34f5
Hexendi <abdellahi.ndiiaye@gmail.com>
2026-04-19 19:01:08 +0000

    rate limit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import rateLimit from 'express-rate-limit';
 import path from "path";
 import cors from "cors";
 import "dotenv/config";
@@ -428,6 +429,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../dist")));
+
+// rate limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Too many requests, please try again later.(2lla bechor)"
+  }
+});
+app.use(limiter);
 
 app.post("/api/ask", async (req, res) => {
   try {
